164a5766c8ac5e9f4df10a644304e9cbec98610c
Sarifa <sarifajusmar@gmail.com>
2023-08-09 15:00:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/firebasecloudmessaging/MyFirebaseMessagingService.kt
@@ -0,0 +1,64 @@
+package com.example.firebasecloudmessaging
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        remoteMessage.notification?.let {
+            generateNotification(it.title ?: "", it.body ?: "")
+        }
+    }
+
+    private fun generateNotification(title: String, message: String) {
+        val channelId = "my_notification_channel"
+        val channelName = "My Notification Channel"
+        val notificationId = System.currentTimeMillis().toInt() // Unique ID for each notification
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.greeting)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Create a notification channel (for Android 8.0 and above)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            // Set vibration pattern for the channel
+            channel.vibrationPattern = longArrayOf(1000, 1000, 1000, 1000)
+
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(notificationId, builder.build())
+    }
+}
